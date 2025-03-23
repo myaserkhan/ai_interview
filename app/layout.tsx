a4f3bf6643f02869f3a06af1ac712c2fe21caab6
Yasir Khan <khanyaser007@gmail.com>
2025-03-23 15:20:51 +0800

    Add Toaster for notifications in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const MonaSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={`${MonaSans.className} antialiased pattern`}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
