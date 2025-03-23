5957fe3f14de073b4e686f1c9583b47f22711523
Yasir Khan <khanyaser007@gmail.com>
2025-03-23 18:08:51 +0800

    Enhance home page with AI interview preparation sections & dynamic interview cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,49 @@
+import InterviewCard from "@/components/InterviewCard";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants/dummydata";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Pratice & Feedback</h2>
+          <p className="text-lg">
+            Practice with real-world questions and get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="Robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} />
+          ))}
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Page;
